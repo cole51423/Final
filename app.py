2fae2d1785c37fa15f6f8b3b16ebc7439632f3a8
cole51423 <39925001+cole51423@users.noreply.github.com>
2025-12-14 05:00:44 +0000

    Dev Container Add

--- app.py
+++ app.py
@@ -46,4 +46,4 @@ def random_params():
     return jsonify({'weights': weights, 'biases': biases})
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(host="0.0.0.0", port=5000, debug=True)
